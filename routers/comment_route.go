c4a2fd1b4efe448e5d5f368e5aff9e8c7eff0ed7
Surawee <surawee.1399200001403@gmail.com>
2023-02-08 15:07:04 +0700

    Can Create Comment in Post

--- routers/comment_route.go
+++ routers/comment_route.go
@@ -8,15 +8,15 @@ import (
 
 func CommentRoutes(route *gin.Engine) {
 	v1 := route.Group("v1/comments")
-	v1.POST("/", controllers.CreateComment)
+	v1.POST("/:post_id", controllers.CreateComment)
 	v1.GET("/:id", controllers.GetAllComment)
-	v1.GET("/id", controllers.GetMyComment)
-	v1.GET("/post/:id", controllers.GetCommentByPostID)
-	v1.GET("/user/:id", controllers.GetCommentByUserID)
-	v1.GET("/:id", controllers.UpdateComment)
-	v1.GET("/like/:id", controllers.DeleteComment)
-	v1.GET("/:id", controllers.LikeComment)
-	v1.GET("/:id", controllers.DislikeComment)
+	// v1.GET("/id", controllers.GetMyComment)
+	// v1.GET("/post/:id", controllers.GetCommentByPostID)
+	// v1.GET("/user/:id", controllers.GetCommentByUserID)
+	// v1.GET("/:id", controllers.UpdateComment)
+	// v1.GET("/like/:id", controllers.DeleteComment)
+	// v1.GET("/:id", controllers.LikeComment)
+	// v1.GET("/:id", controllers.DislikeComment)
 	// v1.POST("/:id", controllers.UpdateComment)
 	// v1.DELETE("/:id", controllers.DeleteComment)
 }
